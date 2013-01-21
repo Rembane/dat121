d3f454873bc9799c07a35c027bf9e23cf72cf29e
Rembane <andeke@gmail.com>
2013-01-21 11:31:06 +0100

    First version of the script. It downloads, commits and pushes!

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 dat121
 ======
 
-This repository keeps the latest version of the Programming Paradigms site, to be able to see the latest changes with git diff.
+This repository keeps the latest version of the org-files of the Programming Paradigms site, so we can see the changes with git diff.
+
+The site: http://www.cse.chalmers.se/~bernardy/pp/
+
---
+++ update.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from urllib import urlretrieve 
+import os.path, subprocess
+
+urls = ['http://www.cse.chalmers.se/~bernardy/pp/index.org',
+'http://www.cse.chalmers.se/~bernardy/pp/Lectures.org',
+'http://www.cse.chalmers.se/~bernardy/pp/Schedule.org',
+'http://www.cse.chalmers.se/~bernardy/pp/pp.css']
+
+for url in urls:
+    urlretrieve(url, os.path.join('site', url.split('/')[-1].strip()))
+
+subprocess.call(['git', 'add', 'site'])
+subprocess.call(['git', 'commit', '-m', u'Autocommit %s' % unicode(datetime.now())])
